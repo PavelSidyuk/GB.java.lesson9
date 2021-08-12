44e46b40a83b089a7f61c4b85de22dfc12d610e2
PavelSidyuk <sidiuk.pavel@yandex.ru>
2021-08-12 21:44:10 +0400

    ДЗ9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,42 @@
 public class Main {
 
 
+    public static void main (String[] args)throws MyArrayDataException, MyArraySizeException {
 
-    
-    public static void main (String[] args) {
+        String[][] strArr = {
+                {"11", "4", "7", "14"},  // в таком виде проще визуально представить двумерный массив 4х4
+                {"33", "2", "1",   "5"},  // в виде строк и колонок
+                {"77", "3", "111", "99"},
+                {"32", "23", "5",  "8"},
 
 
+        };
+
+        initializationArr(strArr);
+    }
+
+
+    public static void initializationArr (String[][] s) throws MyArrayDataException, MyArraySizeException  {
+        int summ = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[i].length; j++) {
+                if (s.length != 4 || s[i].length != 4) {
+                    throw new MyArraySizeException("Не верный размер массива, укажите массив [4][4]" );
+                }
+                try {
+                    Integer.parseInt(s[i][j]);
+                }catch (NumberFormatException e){
+                    throw new MyArrayDataException("Проблемный элемент находится :  линия № " + (i + 1) + "  колонка № " + (j+1) +" Не число ");// прибавляю 1 для того чтобы не програмисты понемали в какой строке и колонке ошибка.
+                }
+
+                summ += Integer.parseInt(s[i][j]);
+            }
+            System.out.println("сумма =" +summ);
+
+
+        }
 
     }
+
 }
---
+++ src/main/java/MyArrayDataException.java
@@ -0,0 +1,5 @@
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException (String s){
+        super(s);
+    }
+}
